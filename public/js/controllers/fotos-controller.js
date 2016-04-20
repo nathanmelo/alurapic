5ca908ff771a240e6da74e65563a31dbbc059a50
nathanmelo <nathan.vix@gmail.com>
2016-04-20 20:24:25 -0300

    Added ngAnimate and debounce on filter.

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,6 +1,7 @@
 angular.module('alurapic').controller('FotosController',function($scope, $http){
 
   $scope.fotos = [];//declared array
+  $scope.filtro = '';
 
   $http.get('v1/fotos') //get requisition to server
   .success(function(fotos){
