4db3f196d7aa731e9d5d01fd2e54b47c3d5c710d
nathanmelo <nathan.vix@gmail.com>
2016-04-20 17:44:22 -0300

    changed the angualarJS 'promisse' to a 'success' call

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -2,11 +2,11 @@ angular.module('alurapic').controller('FotosController',function($scope, $http){
 
   $scope.fotos = [];//declared array
 
-  var promisse = $http.get('v1/fotos'); //get requisition to server
-  promisse.then(function(retorno){
-    $scope.fotos = retorno.data;//if this promisse return something ;)
-  }).catch(function(error){
-    console.log(error);//if the promisse return any error we will log it;
+  $http.get('v1/fotos') //get requisition to server
+  .success(function(fotos){
+    $scope.fotos = fotos; //if this promisse return something ;)
+  }).error(function(error){
+    console.log(error);//if the promisse return an
   });
 
 
