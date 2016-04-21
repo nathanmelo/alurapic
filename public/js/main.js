d6043a79cef597f0f56ca9c40d7ad3abbeca18b6
nathanmelo <nathan.vix@gmail.com>
2016-04-21 12:10:34 -0300

    Configured partial pages

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,18 @@
-angular.module('alurapic',['minhasDiretivas', 'ngAnimate']);
+angular.module('alurapic',['minhasDiretivas', 'ngAnimate', 'ngRoute'])
+.config(function ($routeProvider, $locationProvider){
+
+    $locationProvider.html5Mode(true);//this allow us to ommit # in url
+    
+    $routeProvider.when('/fotos', {
+      templateUrl: 'partials/principal.html',
+      controller: 'FotosController'
+    });
+
+    $routeProvider.when('/fotos/new', {
+      templateUrl: 'partials/foto.html',
+      controller: 'FotosController'
+    });
+
+    $routeProvider.otherwise({redirectTo: '/fotos'});//this is the default route
+
+});
