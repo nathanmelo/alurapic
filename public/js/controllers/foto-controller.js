2298382a2975939fdc4248f64734a15c02ab44d6
nathanmelo <nathan.vix@gmail.com>
2016-04-21 17:33:24 -0300

    Created edit method (http put) for pictures

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,21 +1,54 @@
-angular.module('alurapic').controller('FotoController',function($scope, $http){
+angular.module('alurapic').controller('FotoController',function($scope, $http, $routeParams){
 
+  //our variables
   $scope.foto  = {};
   $scope.mensagem = '';
 
-  $scope.submeter = function(){
+  //here is the editition logic
+  if ($routeParams.fotoId){
+    $http.get('v1/fotos/' + $routeParams.fotoId)
+    .success(function(foto){
+      $scope.foto = foto;
+    })
+      //exception can happen :(
+    .error(function(error){
+      console.log(error);
+      $scope.mensagem = 'Não foi possível obter a foto.';
+    });
+  }
 
+  //here is the include logic
+  $scope.submeter = function(){
+    //if the form do not contain any error
     if($scope.formulario.$valid){
-      $http.post('v1/fotos', $scope.foto)
-      .success(function(){
-        $scope.foto = {};
-        $scope.mensagem = 'Foto incluida com sucesso.';
-        console.log($scope.foto);
-      })
-      .error(function(error){
-        $scope.mensagem = 'Não foi possível incluir a foto.';
-        console.log(error);
-      });
+
+      if($scope.foto._id){
+        //edition function is called
+        $http.put('v1/fotos/'+ $scope.foto._id, $scope.foto)
+        .success(function(){
+          $scope.mensagem = 'A foto '+ $scope.foto.titulo + ' foi alterada com sucesso.';
+        })
+        //exception can happen :(
+        .error(function(error){
+          $scope.mensagem = 'Não foi possível alterar a foto.';
+          console.log(error);
+        })
+
+      }else{
+        //create function is called
+        $http.post('v1/fotos', $scope.foto)
+        .success(function(){
+          //clean the form
+          $scope.foto = {};
+          $scope.mensagem = 'Foto incluida com sucesso.';
+          console.log($scope.foto);
+        })
+        //exception can happen :(
+        .error(function(error){
+          $scope.mensagem = 'Não foi possível incluir a foto.';
+          console.log(error);
+        });
+      }
     }
 
   };
--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -2,6 +2,23 @@ angular.module('alurapic').controller('FotosController',function($scope, $http){
 
   $scope.fotos = [];//declared array
   $scope.filtro = '';
+  $scope.mensagem = '';
+
+  $scope.remover = function(foto){
+
+    console.log(foto);
+    $http.delete('v1/fotos/'+foto._id)
+    .success(function(){
+      var indiceFoto = $scope.fotos.indexOf(foto);
+      $scope.fotos.splice(indiceFoto, 1);
+      $scope.mensagem = 'Foto ' +foto.titulo+ ' removida com sucesso.';
+    })
+    .error(function(error){
+      console.log(error);
+      $scope.mensagem = 'Não foi possível remover a foto '+ foto.titulo;
+    });
+
+  };
 
   $http.get('v1/fotos') //get requisition to server
   .success(function(fotos){
