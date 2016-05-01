4aced123385fc13cec9ed680ba3569aa1206d362
Nathan Melo <nathan.vix@gmail.com>
2016-05-01 18:23:57 -0300

    Change the focus code from the controller to the directive.

--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -50,12 +50,6 @@ angular.module('minhasDiretivas',[])
     ddo.restrict = "A";
     //& = expression, @ = string, = permit control directive e directive control communication (two-way)
 
-      /**
-      //if we don't use watch is not necessary for us to use this
-      ddo.scope = {
-        focado : '='
-      };
-      **/
     //$scope is the scope of controller, this scope is the scope of the directive
     //can acess the scope and the element where our tag (meu-foco) is.
     ddo.link = function(scope, element){
@@ -65,15 +59,3 @@ angular.module('minhasDiretivas',[])
     }
     return ddo;
 });
-      /**
-      //if we use a lot of watches it coud be expensive for load of our page
-      scope.$watch('focado', function(){
-        if(scope.focado){
-          element[0].focus();
-          scope.focado = false;
-        }
-
-
-      });
-      <a href="/" class="btn btn-primary" meu-foco focado="focado">Voltar</a>
-      **/
--- public/js/services/meus-servicos.js
+++ public/js/services/meus-servicos.js
@@ -6,15 +6,18 @@ angular.module('meusServicos', ['ngResource'])
     }
   });
 })
-.factory('cadastroDeFotos', function(recursoFoto, $q){
+.factory('cadastroDeFotos', function(recursoFoto, $q, $rootScope){
   //$q allow us to create a promisse
 
   var servico = {};
+
+  var evento = 'fotoCadastrada';
 
   servico.cadastrar = function(foto){
     return $q(function(resolve, reject){
       if(foto._id){
         recursoFoto.update({fotoId: foto._id}, foto, function(){
+          $rootScope.$broadcast(evento);
           resolve({
             mensagem : 'Foto '+ foto.titulo + ' atualizada com sucesso!',
             inclusao : false
@@ -27,6 +30,7 @@ angular.module('meusServicos', ['ngResource'])
         });
       }else{
         recursoFoto.save(foto, function(){
+          $rootScope.$broadcast(evento);
           resolve({
             mensagem : 'Foto '+ foto.titulo + ' incluida com sucesso',
             inclusao : true
